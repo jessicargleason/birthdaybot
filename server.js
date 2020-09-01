1f25f43cfd0324a63bf82fdc075b9cae02b0f784
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-08-31 20:37:35 -0500

    Progress toward promises

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const handlebars = require('express-handlebars');
 const request = require('request');
+const helmet = require('helmet');
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.json());
+app.use(helmet());
 app.use(express.static("express"));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -21,8 +23,8 @@ const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
 
-const results = () => {
-  return "Test";
+let results = {
+  error: false
 }
 
 //Sets our app to use the handlebars engine
@@ -32,22 +34,77 @@ layoutsDir: __dirname + '/views/layouts',
 }));
 
 //Serves static files (we need it to import a css file)
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/birthdays/', (req, res) => {
   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
   res.render('main', {layout : 'index'});
 });
 
-app.post('/', function(req, res) {
-  formatName(req.body.name);
-  console.log('You sent the name "' + req.body.name + '".');
-  res.render('results', {layout : 'index', results: results()});
+app.post('/birthdays/', (req, res) => {
+  results = {};
+  
+  results.name = req.body.name;
+  formatName(req.body.name).then(() => {
+    res.render('results', {layout : 'index', results: results});
+  })
+  .catch(function (e) {
+    res.status(500, {
+          error: e
+    });
+  });
+  return setUpResults;
+    
 });
+
+function setUpResults(name) {
+  const formattedName = formatName(name);
+  console.log(formattedName);
+  //const artistId = getArtistId(formattedName);
+  //console.log(artistId);
+  //getArtist(artistId);
+  /* if (artistId !== false) {
+    let body = getArtist(artistId);
+    //console.log(body);
+    //body = JSON.parse(body);
+    // Go through the data and find the birth date (P569)
+    if (body.entities[id].claims.P569 !== undefined) {
+      let birthday = body.entities[id].claims.P569[0].mainsnak.datavalue.value.time;
+      //Stop JS from trying to do time zone conversion
+      birthday = birthday.split('T')[0];
+      birthday = birthday.replace('+','');
+      birthday = birthday.replace(/\-/g,'/');
+      results.birthday = birthday;
+      birthday = new Date(birthday);
+      results.month = (birthday.getMonth() + 1).toString();
+      results.day = birthday.getDate().toString();
+    } else {
+
+      results.error = true;
+      return false;
+    } */
+    /* Spotify ID (P1902) */
+    /* if (body.entities[id].claims.P1902 === undefined) {
+
+      console.log("no spotify");
+      render.spotify = false;
+      return false;
+    } else {
+    var spotify = body.entities[id].claims.P1902[0].mainsnak.datavalue.value;
+
+      //getSpotify(spotify);
+    } 
+  } else {
+    //can't find person
+  }*/
+  return Promise.resolve();
+}
 
 function formatName(name) {
   var formattedName = name.split(' ').join('+');
   getArtistId(formattedName);
+  //return formattedName;
+  //getArtistId(formattedName);
 }
 
 function getArtistId(formattedName) {
@@ -63,11 +120,16 @@ function getArtistId(formattedName) {
       body = JSON.parse(body);
       if (body.search[0] !== undefined) {
         var id = body.search[0].id;
-        console.log(id);
+        //return id;
+        //console.log(id);
         getArtist(id);
+        } else {
+          results.error = true;
+          return false;
         }
     });
   });
+}
 
   /* Use Wikidata to get that artist's data using their ID */
 	
@@ -82,36 +144,30 @@ function getArtistId(formattedName) {
       });
       res.on("end", () => {
         body = JSON.parse(body);
-        console.log(body);
+        console.log(body.entities[id].claims.P569);
         console.log("Success!");
-        /* Go through the data and find the birth date (P569) */
+        // Go through the data and find the birth date (P569)
         if (body.entities[id].claims.P569 !== undefined) {
-        var birthday = body.entities[id].claims.P569[0].mainsnak.datavalue.value.time;
-          console.log(birthday);
+          let birthday = body.entities[id].claims.P569[0].mainsnak.datavalue.value.time;
+          //Stop JS from trying to do time zone conversion
+          birthday = birthday.split('T')[0];
+          birthday = birthday.replace('+','');
+          birthday = birthday.replace(/\-/g,'/');
+          results.birthday = birthday;
+          birthday = new Date(birthday);
+          results.month = (birthday.getMonth() + 1).toString();
+          results.day = birthday.getDate().toString();
+          return Promise.resolve();
         } else {
-          /* var text = document.getElementById("birthday");
-      
-          text.textContent = "Birthday Unknown"; */
+          results.error = true;
+          return false;
         }
-        /* Spotify ID (P1902) */
-        if (body.entities[id].claims.P1902 === undefined) {
-          console.log("no spotify");
-          /* var nameText = document.getElementById("artist").textContent = "Spotify Unknown";
-          var genresText = document.getElementById("genres").textContent = "";
-          var linkText = document.getElementById("link").textContent = ""; */
-        } else {
-        var spotify = body.entities[id].claims.P1902[0].mainsnak.datavalue.value;
-  
-          console.log(spotify);
-  
-          getSpotify(spotify);
-        }
+        
       });
     });
 		
 	}
  
-}
 
 function getSpotify(id) {
   var requestURL = "https://api.spotify.com/v1/artists/" + id;
@@ -140,68 +196,16 @@ function getSpotify(id) {
         json: true
       };
       request.get(options, function(error, response, body) {
-        console.log(body["name"]);
+        results.spotifyLink = body["external_urls"]["spotify"];
+        results.genres = body["genres"];
+        results.followers = body["followers"]["total"].toString();
+        console.log(results);
+        spotify = true;
+        return;
       });
     }
   });
-
-/*   
-  $.ajax({
-    method: "GET",
-    url: requestURL,
-    success: function(data) {
-    var name = data["name"];
-    console.log(name);
-    var genres = data["genres"];
-    var link = data["external_urls"]["spotify"];
-    var nameText = document.getElementById("artist").textContent = name;
-    var genresText = document.getElementById("genres").textContent = genres;
-    var linkText = document.getElementById("link").textContent = link;
-    }
-  }); */
   
 }
 
 console.debug('Server listening on port ' + port);
-
-  /* Load the HTTP library */
-/* var http = require("http");
-var request = require('request');
-var client_id = process.env.CLIENT_ID;
-var client_secret = process.env.CLIENT_SECRET;
-
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/artists/7crPfGd2k81ekOoSqQKWWz',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body["name"]);
-    });
-  }
-});
-
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.write("Client ID: " + process.env.CLIENT_ID);
-    response.end();
-  }).listen(8888); */
