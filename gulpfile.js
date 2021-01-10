8baa1aa0022be57f673984f3a89fd3eff7cc22ba
Jessica Gleason <jessicaruthgleason@gmail.com>
2021-01-10 16:19:03 -0600

    Install Tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var tailwindcss = require('tailwindcss');
 
 
 gulp.task('styles', function(){
     return gulp.src('src/style.css')
-     .pipe(postcss([cssImport, nested, autoprefixer]))
+     .pipe(postcss([cssImport, nested, autoprefixer, tailwindcss]))
      .pipe(gulp.dest('public'))
    });
